dabc02376d63f22daa90b8087f899d82c9aa6f7b
Seiji Naganuma <Snaganumas@gmail.com>
2015-03-21 15:46:28 -0400

    finish ajax feature and start dashboard format

---
+++ app/assets/javascripts/myjs.js
@@ -0,0 +1,46 @@
+$(function(){
+  submitTask();
+  deleteTask();
+});
+
+function deleteTask(){
+  $(".task_delete").on('click', deleteExistingTask);
+}
+
+function deleteExistingTask(e){
+  var link = $(this);
+  stopAction(e);
+  $.ajax({
+      url: link.attr("href"),
+      method: 'DELETE',
+      context: this,
+      success: function(result) {
+        link.parent().remove();
+        link.href = this.href;
+      }
+  });
+
+}
+
+function submitTask(){
+  $("#new_task").on('submit', createNewTask);
+}
+
+function createNewTask(e){
+  stopAction(e);
+  submitForm($(this), function(savedTask){
+    $("#task_name").val("");
+    $("#tasks").prepend($(savedTask));
+    deleteTask();
+  });
+}
+
+function stopAction(e){
+  e.preventDefault();
+}
+
+function submitForm(form, callbackAction){
+  var action = form.attr("action");
+  var params = form.serialize();
+  $.post(action, params, callbackAction);
+}
